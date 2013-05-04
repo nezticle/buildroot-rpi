92479fa11e54ae3b604233919b51e196dc891221
albertd <albert@dahan.nl>
2013-05-04 09:50:37 +0200

    rpi-userland: bump to latest git

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RPI_USERLAND_VERSION = a158f9ab70eb217b5ee72ebd83c1536418df7c7c
+RPI_USERLAND_VERSION = 78f10539fa17f0601892ae3fe2f2db42f175eaa2
 RPI_USERLAND_SITE = http://github.com/raspberrypi/userland/tarball/master
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILE = LICENCE
@@ -12,7 +12,8 @@ RPI_USERLAND_INSTALL_STAGING = YES
 RPI_USERLAND_CONF_OPT = -DVMCS_INSTALL_PREFIX=/usr
 
 define RPI_USERLAND_POST_TARGET_CLEANUP
-    rm -Rf $(TARGET_DIR)/usr/src
+	rm -f $(TARGET_DIR)/etc/init.d/vcfiled
+	rm -Rf $(TARGET_DIR)/usr/src
 endef
 
 RPI_USERLAND_POST_INSTALL_TARGET_HOOKS += RPI_USERLAND_POST_TARGET_CLEANUP
