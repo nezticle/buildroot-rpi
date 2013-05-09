4b7d1d8b2a6550c9fbd9cd700b356d8bdb40863e
albertd <albert@dahan.nl>
2013-05-10 00:30:57 +0200

    gst-omx: move repository

--- package/multimedia/gst-omx/gst-omx.mk
+++ package/multimedia/gst-omx/gst-omx.mk
@@ -3,11 +3,8 @@
 # gst-omx
 #
 #############################################################
-#GST_OMX_VERSION = 28beb68797c6a260a37bedfa0c0d6026b3dd0a84
-#GST_OMX_SITE = http://cgit.freedesktop.org/gstreamer/gst-omx/snapshot/
 GST_OMX_VERSION = 07b2fe65d3bcf72d38bbf24453d493d38c359d9c
-GST_OMX_SITE = http://cgit.freedesktop.org/~adn770/gst-omx/snapshot/
-GST_OMX_SOURCE = gst-omx-$(GST_OMX_VERSION).tar.gz
+GST_OMX_SITE = http://github.com/albertd/gst-omx/tarball/master
 GST_OMX_INSTALL_STAGING = YES
 
 GST_OMX_POST_DOWNLOAD_HOOKS += GSTREAMER_COMMON_DOWNLOAD
