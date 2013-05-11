c40330f229735bc5783405191825d70e722c9c44
albertd <albert@dahan.nl>
2013-05-11 11:09:53 +0200

    gst-fludash: correct links and install of fludownloader

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -11,11 +11,13 @@ GST_FLUDASH_DEPENDENCIES += libcurl
 
 define GST_FLUDASH_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10
-	$(INSTALL) -m 755 $(@D)/usr/lib/libfludownloader.so $(TARGET_DIR)/usr/lib/
-	cp $(@D)/usr/lib/libfludownloader.so.0 $(TARGET_DIR)/usr/lib/
-	cp $(@D)/usr/lib/libfludownloader.so.0.0.0 $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -m 755 $(@D)/usr/lib/libfludownloader.so $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
 	$(INSTALL) -m 755 $(@D)/usr/lib/gstreamer-0.10/libgstfluaacdec.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
 	$(INSTALL) -m 755 $(@D)/usr/lib/gstreamer-0.10/libgstfludash.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
+	(cd $(TARGET_DIR)/usr/lib/; \
+		ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
+		ln -sfn libfludownloader.so.0 libfludownloader.so; \
+	)
 endef
 
 $(eval $(generic-package))
