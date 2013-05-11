65e4cc912c1a7bb0da713a48b1a9aea83f924345
albertd <albert@dahan.nl>
2013-05-11 14:55:07 +0200

    gst-fludash: enable by default

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -12,14 +12,18 @@ GST_FLUDASH_REDISTRIBUTE = NO
 GST_FLUDASH_DEPENDENCIES += libcurl
 
 define GST_FLUDASH_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10
-	$(INSTALL) -m 755 $(@D)/fludownloader.bin $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
-	$(INSTALL) -m 755 $(@D)/gstfluaacdec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluaacdec.so
-	$(INSTALL) -m 755 $(@D)/gstfludash.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfludash.so
-	(cd $(TARGET_DIR)/usr/lib/; \
-		ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
-		ln -sfn libfludownloader.so.0 libfludownloader.so; \
-	)
+	if [ ! -f $(@D)/gstfludash.bin ]; then \
+		cat $(@D)/README; \
+	else \
+		mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10; \
+		$(INSTALL) -m 755 $(@D)/fludownloader.bin $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0; \
+		$(INSTALL) -m 755 $(@D)/gstfluaacdec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluaacdec.so; \
+		$(INSTALL) -m 755 $(@D)/gstfludash.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfludash.so; \
+		(cd $(TARGET_DIR)/usr/lib/; \
+			ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
+			ln -sfn libfludownloader.so.0 libfludownloader.so; \
+		) \
+	fi
 endef
 
 $(eval $(generic-package))
