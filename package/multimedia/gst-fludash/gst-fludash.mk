fe0a1d6d4d266cfd726ce48ce3cf2c7b33220d74
albertd <albert@dahan.nl>
2013-05-11 14:00:45 +0200

    gst-fludash: rename proprietary libraries to work with rsync

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -11,9 +11,9 @@ GST_FLUDASH_DEPENDENCIES += libcurl
 
 define GST_FLUDASH_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10
-	$(INSTALL) -m 755 $(@D)/libfludownloader.so $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
-	$(INSTALL) -m 755 $(@D)/libgstfluaacdec.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
-	$(INSTALL) -m 755 $(@D)/libgstfludash.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
+	$(INSTALL) -m 755 $(@D)/fludownloader.bin $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
+	$(INSTALL) -m 755 $(@D)/gstfluaacdec.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfluaacdec.so
+	$(INSTALL) -m 755 $(@D)/gstfludash.bin $(TARGET_DIR)/usr/lib/gstreamer-0.10/libgstfludash.so
 	(cd $(TARGET_DIR)/usr/lib/; \
 		ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
 		ln -sfn libfludownloader.so.0 libfludownloader.so; \
