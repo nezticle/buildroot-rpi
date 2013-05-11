6ec883e026b577238bd6c627c649950d7636e6e6
albertd <albert@dahan.nl>
2013-05-11 13:51:31 +0200

    gst-fludash: move to folder instead of intranet

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -4,16 +4,16 @@
 #
 #############################################################
 GST_FLUDASH_VERSION = 1.0.0
-GST_FLUDASH_SOURCE = gst-fludash-$(GST_FLUDASH_VERSION).tar.gz
-GST_FLUDASH_SITE = http://build.metrological.com/rpi/
+GST_FLUDASH_SITE = $(TOPDIR)/package/multimedia/gst-fludash/libs
+GST_FLUDASH_SITE_METHOD = local
 
 GST_FLUDASH_DEPENDENCIES += libcurl
 
 define GST_FLUDASH_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10
-	$(INSTALL) -m 755 $(@D)/usr/lib/libfludownloader.so $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
-	$(INSTALL) -m 755 $(@D)/usr/lib/gstreamer-0.10/libgstfluaacdec.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
-	$(INSTALL) -m 755 $(@D)/usr/lib/gstreamer-0.10/libgstfludash.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
+	$(INSTALL) -m 755 $(@D)/libfludownloader.so $(TARGET_DIR)/usr/lib/libfludownloader.so.0.0.0
+	$(INSTALL) -m 755 $(@D)/libgstfluaacdec.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
+	$(INSTALL) -m 755 $(@D)/libgstfludash.so $(TARGET_DIR)/usr/lib/gstreamer-0.10/
 	(cd $(TARGET_DIR)/usr/lib/; \
 		ln -sfn libfludownloader.so.0.0.0 libfludownloader.so.0; \
 		ln -sfn libfludownloader.so.0 libfludownloader.so; \
