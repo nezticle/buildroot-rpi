aa5f90859188dc397cdda302bb33225a6adabbfb
albertd <albert@dahan.nl>
2013-05-11 11:00:56 +0200

    gst-fludash: adding dependency on libcurl

--- package/multimedia/gst-fludash/gst-fludash.mk
+++ package/multimedia/gst-fludash/gst-fludash.mk
@@ -7,6 +7,8 @@ GST_FLUDASH_VERSION = 1.0.0
 GST_FLUDASH_SOURCE = gst-fludash-$(GST_FLUDASH_VERSION).tar.gz
 GST_FLUDASH_SITE = http://build.metrological.com/rpi/
 
+GST_FLUDASH_DEPENDENCIES += libcurl
+
 define GST_FLUDASH_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/gstreamer-0.10
 	$(INSTALL) -m 755 $(@D)/usr/lib/libfludownloader.so $(TARGET_DIR)/usr/lib/
