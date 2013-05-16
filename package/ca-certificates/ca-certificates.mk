db80d744082080996351b12ad9138a5aaaa462ed
albertd <albert@dahan.nl>
2013-05-16 14:24:33 +0200

    ca-certificates: copy to correct path

--- package/ca-certificates/ca-certificates.mk
+++ package/ca-certificates/ca-certificates.mk
@@ -9,10 +9,10 @@ CA_CERTIFICATES_SITE_METHOD = local
 
 define CA_CERTIFICATES_INSTALL_TARGET_CMDS
 	$(@D)/mkcabundle.pl > $(@D)/ca-bundle.crt
-	mkdir -p $(TARGET_DIR)/usr/lib/ssl/certs
-	mkdir -p $(TARGET_DIR)/usr/lib/ssl/CA/private
-	cp $(@D)/ca-bundle.crt $(TARGET_DIR)/usr/lib/ssl/certs/
-	(cd $(TARGET_DIR)/usr/lib/ssl; \
+	mkdir -p $(TARGET_DIR)/etc/ssl/certs
+	mkdir -p $(TARGET_DIR)/etc/ssl/CA/private
+	cp $(@D)/ca-bundle.crt $(TARGET_DIR)/etc/ssl/certs/
+	(cd $(TARGET_DIR)/etc/ssl; \
 		ln -sf certs/ca-bundle.crt cert.pem; \
 	)
 endef
