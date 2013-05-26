f901749829d023fa3401ca16310018415a5747c8
albertd <albert@dahan.nl>
2013-05-26 16:01:45 +0200

    wayland: add rpi dependency

--- package/wayland/wayland.mk
+++ package/wayland/wayland.mk
@@ -7,6 +7,10 @@
 WAYLAND_VERSION = 1.1.0
 WAYLAND_SITE = http://wayland.freedesktop.org/releases/
 WAYLAND_SOURCE = wayland-$(WAYLAND_VERSION).tar.xz
+#WAYLAND_VERSION = d68c7d8aed92158ae68fd9cabb8bc2a50fd5066b
+#WAYLAND_SITE = http://cgit.freedesktop.org/wayland/wayland/snapshot/
+#WAYLAND_SOURCE = wayland-$(WAYLAND_VERSION).tar.gz
+#WAYLAND_AUTORECONF = YES
 WAYLAND_LICENSE = MIT
 WAYLAND_LICENSE_FILES = COPYING
 
@@ -19,10 +23,15 @@ WAYLAND_DEPENDENCIES = libffi host-pkgconf expat host-expat
 # that the tool is already available.
 WAYLAND_CONF_OPT = --disable-scanner
 
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+WAYLAND_DEPENDENCIES += rpi-userland
+endif
+
 define WAYLAND_BUILD_SCANNER
 	(cd $(@D)/src/; \
 		$(HOSTCC) $(HOST_CFLAGS) $(HOST_LDFLAGS) \
 			-o wayland-scanner scanner.c wayland-util.c -lexpat)
+	cp -f $(@D)/src/wayland-scanner $(HOST_DIR)/usr/bin/
 endef
 
 WAYLAND_POST_CONFIGURE_HOOKS += WAYLAND_BUILD_SCANNER
