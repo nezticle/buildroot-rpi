043466e3fa1c0375aa455a19f90092e0e30b0e24
rbp <m.sieben@metrological.com>
2013-05-28 11:16:51 +0200

    libcec-uinput : bump version

--- package/libcec-uinput/libcec-uinput.mk
+++ package/libcec-uinput/libcec-uinput.mk
@@ -3,7 +3,7 @@
 # libcec-uinput
 #
 #############################################################
-LIBCEC_UINPUT_VERSION = cb086865869402fdabe3b9142ea7e6ea3c699c1b
+LIBCEC_UINPUT_VERSION = 70b3f2e599f8805c5a9e6d15be3078ba25c433f8
 LIBCEC_UINPUT_SITE = git://github.com/bramp/libcec-daemon.git
 LIBCEC_UINPUT_INSTALL_STAGING = YES
 LIBCEC_UINPUT_INSTALL_TARGET = YES
