d860b9d2cbb519813533660fb1d1a6ad04cdace3
albertd <albert@dahan.nl>
2013-06-17 21:47:57 +0200

    rpi-userland: bump version

--- package/rpi-userland/rpi-userland.mk
+++ package/rpi-userland/rpi-userland.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-RPI_USERLAND_VERSION = 986774edf7c91a8a91d59abadbdb0b34b5a387dc
+RPI_USERLAND_VERSION = 725b03007b0f53525767539bb435bdcfeed5ea66
 RPI_USERLAND_SITE = http://github.com/raspberrypi/userland/tarball/master
 RPI_USERLAND_LICENSE = BSD-3c
 RPI_USERLAND_LICENSE_FILES = LICENCE
